3ae95e82d0b468f66ae23720293c7777de0b2e55
SerenaLisio <37556574+SerenaLisio@users.noreply.github.com>
2018-04-06 09:48:28 -0400

    Idler Shaft Update

--- Plotting.m
+++ Plotting.m
@@ -4,12 +4,12 @@ clear all
 
 Gearbox_Parameters
 Forces_Torques
-Input_Shaft
+Idler_Shaft
 
 x = 0:0.01:gearbox.shaft.length;
 
 for i = 1:length(x)
-    VB = IN_BM(x(i),gearbox.pinion2.geometry.position(1),gearbox.pinion1.geometry.position(1),gearbox.shaft.length,gearbox.pinion2.loads.FW,gearbox.pinion1.loads.FW,gearbox.shaft.IN.ROY,gearbox.shaft.IN.ROZ,gearbox.shaft.IN.RLY,gearbox.shaft.IN.RLZ,gearbox.shaft.IN.torsion);
+    VB = ID_BM(x(i),gearbox.gear2.geometry.position(1),gearbox.pinion3.geometry.position(1),gearbox.shaft.length,gearbox.gear2.loads.FW,gearbox.pinion3.loads.FW,gearbox.shaft.ID.ROY,gearbox.shaft.ID.ROZ,gearbox.shaft.ID.RLY,gearbox.shaft.ID.RLZ,gearbox.shaft.ID.torsion);
     V(i,1:3) = [x(i);VB(1,1);VB(1,2)];
     B2(i,1:3) = [x(i);VB(2,1);VB(2,2)];
 end
@@ -21,9 +21,9 @@ figure(2)
 plot(V(:,1),V(:,3))
 
 figure(3)
-plot(B2(:,1),((B2(:,2).^2)+(B2(:,3).^2)).^.5)
-hold on
-plot(B2(:,1),B2(:,2))
-hold on
+% plot(B2(:,1),((B2(:,2).^2)+(B2(:,3).^2)).^.5)
+% hold on
+% plot(B2(:,1),B2(:,2))
+% hold on
 plot(B2(:,1),B2(:,3))
 
