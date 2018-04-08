5f26f3bf7f057f14a33c77db7d4e0c666b9ac8db
Felix Lamy <felix.lamy@gmail.com>
2018-04-08 15:29:48 -0400

    Fixed Bug in Shafts
    Output shaft calcs were commented out!

--- Shafts.m
+++ Shafts.m
@@ -45,46 +45,46 @@ eqn2 = gearbox.gear2.loads.FW + gearbox.pinion3.loads.FW + gearbox.shaft.ID.bear
 
 gearbox.shaft.ID.bearing1.loads.F = [0; double((vpa(solRay))); double((vpa(solRaz)))];
 
-% %% Output Shaft 1
-% 
-% syms Rby Rbz
-% 
-% gearbox.shaft.O1.bearing2.loads.F = [0; Rby; Rbz];
-% gearbox.shaft.O1.bearing2.geometry.position = [gearbox.shaft.O1.geometry.length; 0; 0];
-% 
-% eqn = cross(gearbox.gear1.geometry.position, gearbox.gear1.loads.FW) + cross(gearbox.shaft.O1.bearing2.geometry.position, gearbox.shaft.O1.bearing2.loads.F) == 0;
-% [solRby,solRbz] = solve(eqn);
-% 
-% gearbox.shaft.O1.bearing2.loads.F = [0; double((vpa(solRby))); double((vpa(solRbz)))];
-% 
-% syms Ray Raz
-% gearbox.shaft.O1.bearing1.loads.F = [0; Ray; Raz];
-% gearbox.shaft.O1.bearing1.geometry.position = [0; 0; 0];
-% 
-% eqn = gearbox.gear1.loads.FW + gearbox.shaft.O1.bearing2.loads.F + gearbox.shaft.O1.bearing1.loads.F == 0;
-% [solRay,solRaz] = solve(eqn);
-% 
-% gearbox.shaft.O1.bearing1.loads.F = [0; double((vpa(solRay))); double((vpa(solRaz)))];
+%% Output Shaft 1
 
-% %% Output 2
-% 
-% syms Rby Rbz
-% 
-% gearbox.shaft.O2.bearing2.loads.F = [0; Rby; Rbz];
-% gearbox.shaft.O2.bearing2.geometry.position = [gearbox.shaft.O2.geometry.length; 0; 0];
-% 
-% eqn = cross(gearbox.gear3.geometry.position, gearbox.gear3.loads.FW) + cross(gearbox.shaft.O2.bearing2.geometry.position, gearbox.shaft.O2.bearing2.loads.F) == 0;
-% [solRby,solRbz] = solve(eqn);
-% 
-% gearbox.shaft.O2.bearing2.loads.F = [0; double((vpa(solRby))); double((vpa(solRbz)))];
-% 
-% syms Ray Raz
-% gearbox.shaft.O2.bearing1.loads.F = [0; Ray; Raz];
-% gearbox.shaft.O2.bearing1.position = [0; 0; 0];
-% 
-% eqn = gearbox.gear3.loads.FW + gearbox.shaft.O2.bearing2.loads.F + gearbox.shaft.O2.bearing1.loads.F == 0;
-% [solRay,solRaz] = solve(eqn);
-% 
-% gearbox.shaft.O2.bearing1.loads.F = [0; double((vpa(solRay))); double((vpa(solRaz)))];
+syms Rby Rbz
+
+gearbox.shaft.O1.bearing2.loads.F = [0; Rby; Rbz];
+gearbox.shaft.O1.bearing2.geometry.position = [gearbox.shaft.O1.geometry.length; 0; 0];
+
+eqn = cross(gearbox.gear1.geometry.position, gearbox.gear1.loads.FW) + cross(gearbox.shaft.O1.bearing2.geometry.position, gearbox.shaft.O1.bearing2.loads.F) == 0;
+[solRby,solRbz] = solve(eqn);
+
+gearbox.shaft.O1.bearing2.loads.F = [0; double((vpa(solRby))); double((vpa(solRbz)))];
+
+syms Ray Raz
+gearbox.shaft.O1.bearing1.loads.F = [0; Ray; Raz];
+gearbox.shaft.O1.bearing1.geometry.position = [0; 0; 0];
+
+eqn = gearbox.gear1.loads.FW + gearbox.shaft.O1.bearing2.loads.F + gearbox.shaft.O1.bearing1.loads.F == 0;
+[solRay,solRaz] = solve(eqn);
+
+gearbox.shaft.O1.bearing1.loads.F = [0; double((vpa(solRay))); double((vpa(solRaz)))];
+
+%% Output 2
+
+syms Rby Rbz
+
+gearbox.shaft.O2.bearing2.loads.F = [0; Rby; Rbz];
+gearbox.shaft.O2.bearing2.geometry.position = [gearbox.shaft.O2.geometry.length; 0; 0];
+
+eqn = cross(gearbox.gear3.geometry.position, gearbox.gear3.loads.FW) + cross(gearbox.shaft.O2.bearing2.geometry.position, gearbox.shaft.O2.bearing2.loads.F) == 0;
+[solRby,solRbz] = solve(eqn);
+
+gearbox.shaft.O2.bearing2.loads.F = [0; double((vpa(solRby))); double((vpa(solRbz)))];
+
+syms Ray Raz
+gearbox.shaft.O2.bearing1.loads.F = [0; Ray; Raz];
+gearbox.shaft.O2.bearing1.position = [0; 0; 0];
+
+eqn = gearbox.gear3.loads.FW + gearbox.shaft.O2.bearing2.loads.F + gearbox.shaft.O2.bearing1.loads.F == 0;
+[solRay,solRaz] = solve(eqn);
+
+gearbox.shaft.O2.bearing1.loads.F = [0; double((vpa(solRay))); double((vpa(solRaz)))];
 
 
